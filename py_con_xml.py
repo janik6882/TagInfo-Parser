b1631ab8a1a9d489be0cb347d53ebf4b21f35048
Janik <janik@klauenberg.eu>
2020-10-10 13:01:47 +0200

    maded pep8 compatible

--- py_con_xml.py
+++ py_con_xml.py
@@ -1,6 +1,8 @@
-import json
+# -*- coding: utf-8 -*-
 import xmltodict
 import os
+
+
 def get_data(path):
     x = open(path, "r")
     x = xmltodict.parse(x.read())
@@ -14,12 +16,13 @@ def get_data(path):
         res += str(i)
     return res
 
+
 def main():
     p1 = os.path.join("scans", "SFL_2012.xml")
     p2 = os.path.join("scans", "SFL_2017.xml")
     p3 = os.path.join("scans", "SFL_2018_adv_night.xml")
     x2012 = get_data(p1)
-    x2017 =  get_data(p2)
+    x2017 = get_data(p2)
     x2018 = get_data(p3)
     print len(x2012)
     print x2012
@@ -27,6 +30,7 @@ def main():
     print x2017
     print len(x2018)
     print x2018
+
 
 if __name__ == '__main__':
     main()
